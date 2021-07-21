3e8cd29bb19eec0841c0091aa8b350e5c0e963ec
Dario Diaz <dario.dad@gmail.com>
2021-07-21 18:52:58 -0300

    Add code to handle generics in basepage class

--- src/com/itx/framework/base/BasePage.java
+++ src/com/itx/framework/base/BasePage.java
@@ -8,4 +8,13 @@ public abstract class BasePage extends Base {
     public BasePage() {
         PageFactory.initElements(DriverContext.Driver, this);
     }
+
+    public <TPage extends BasePage> TPage As(Class<TPage> pageInstance) {
+        try {
+            return (TPage) this;
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return null;
+    }
 }
