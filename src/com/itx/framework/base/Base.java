d00dd693e8b81250ad1f9d6b5bbcf9283f479472
Dario Diaz <dario.dad@gmail.com>
2021-07-21 18:50:11 -0300

    Add generics to base class

--- src/com/itx/framework/base/Base.java
+++ src/com/itx/framework/base/Base.java
@@ -1,6 +1,14 @@
 package com.itx.framework.base;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class Base {
 
     public static BasePage CurrentPage;
+
+    public <TPage extends BasePage> TPage GetInstance(Class<TPage> page) {
+        Object obj = PageFactory.initElements(DriverContext.Driver, page);
+        return page.cast(obj);
+
+    }
 }
